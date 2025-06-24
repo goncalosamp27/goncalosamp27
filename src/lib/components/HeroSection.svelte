a34de361bdb42a45b89b4bd8606b23983359f96b
goncalosamp27 <goncalo20.sampaio03@gmail.com>
2025-06-24 20:28:35 +0100

    organizing code

--- src/lib/components/HeroSection.svelte
+++ src/lib/components/HeroSection.svelte
@@ -1,22 +1,15 @@
 <script lang="ts"> 
-    import { currentTheme } from '$lib/stores/theme';
+    import { currentTheme, type ThemeName } from '$lib/stores/theme';
     import { fly, fade } from 'svelte/transition';
     import { onMount } from 'svelte';
 
     let open = false;
-    onMount(() => {
-        open = true;
-    });
-
     let showThemes = false;
-    $: currentTheme.subscribe(value => $currentTheme = value);
 
-    function toggleModeMenu () {
-        showThemes = !showThemes;
-    }
-    function switchTo(theme: 'ocean' | 'candy') {
-        currentTheme.set(theme);
-    }
+    onMount(() => { open = true; });
+    $: currentTheme.subscribe(value => $currentTheme = value);
+    function toggleModeMenu () { showThemes = !showThemes; }
+    function switchTo(theme: ThemeName) { currentTheme.set(theme); }
 
 </script>
 
@@ -28,7 +21,7 @@
 
 {#if open}
 <header class="w-full px-15 py-10 relative flex items-center justify-between" in:fade={{ duration: 1500 }}>
-    <button on:click={() => location.reload()} class="w-12 h-12 text-[var(--white)] hover:text-[var(--hover)] transition-colors duration-300 cursor-pointer" aria-label = "logo">
+    <button on:click={() => location.href = '/'} class="w-12 h-12 text-[var(--white)] hover:text-[var(--hover)] transition-colors duration-300 cursor-pointer" aria-label = "logo">
         <svg viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-full h-full">
             <path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M31.9646 0L39.674 13.353L54.5671 9.36241L50.5765 24.2554L63.9292 31.9645L50.5765 39.6737L54.5672 54.5671L39.6739 50.5764L31.9647 63.9292L24.2554 50.5763L9.36203 54.567L13.3527 39.6737L0 31.9646L13.3529 24.2553L9.36229 9.36217L24.2554 13.3528L31.9646 0ZM31.8661 50.3147C41.946 50.3147 50.1174 42.1433 50.1174 32.0633C50.1174 21.9833 41.946 13.8119 31.8661 13.8119C21.7861 13.8119 13.6147 21.9833 13.6147 32.0633C13.6147 42.1433 21.7861 50.3147 31.8661 50.3147Z"/>
             <circle cx="32" cy="32" r="14" fill="currentColor"/>
@@ -88,7 +81,7 @@
 
                 {#if $currentTheme !== 'candy'}
                 <button on:click={() => switchTo('candy')} aria-label="Candy" title="Candy" class="w-12 h-12">
-                    <svg viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-full h-full"> <path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M31.9646 0L39.674 13.353L54.5671 9.36241L50.5765 24.2554L63.9292 31.9645L50.5765 39.6737L54.5672 54.5671L39.6739 50.5764L31.9647 63.9292L24.2554 50.5763L9.36203 54.567L13.3527 39.6737L0 31.9646L13.3529 24.2553L9.36229 9.36217L24.2554 13.3528L31.9646 0ZM31.8661 50.3147C41.946 50.3147 50.1174 42.1433 50.1174 32.0633C50.1174 21.9833 41.946 13.8119 31.8661 13.8119C21.7861 13.8119 13.6147 21.9833 13.6147 32.0633C13.6147 42.1433 21.7861 50.3147 31.8661 50.3147Z"/><circle cx="32" cy="32" r="14" fill="currentColor" /></svg>
+                    candy svg
                 </button>
                 {/if}
             </div>
