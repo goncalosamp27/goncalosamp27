e4bd2bd6a9d29b038e57d1ec21d575643cc8e503
goncalosamp27 <goncalo20.sampaio03@gmail.com>
2025-06-24 02:29:50 +0100

    Started hero section - header + title

---
+++ src/lib/components/HeroSection.svelte
@@ -0,0 +1,69 @@
+<script> 
+    import { fly, fade } from 'svelte/transition';
+    import { onMount } from 'svelte';
+
+    let show = false;
+    onMount(() => {
+        show = true;
+    });
+
+</script>
+
+<section
+  class="min-h-screen flex flex-col"
+  style="background: linear-gradient(var(--ocean-blue) 0%, var(--ocean-blue) 40%, var(--ocean-yellow) 100%); 
+        color: var(--white)"
+>
+
+{#if show}
+<header class="w-full px-12 py-8 relative flex items-center justify-between">
+    <div class="w-12 h-12 text-[var(--white)] hover:text-[var(--ocean-yellow)] transition-colors duration-300 cursor-pointer">
+        <svg viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-full h-full">
+            <path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M31.9646 0L39.674 13.353L54.5671 9.36241L50.5765 24.2554L63.9292 31.9645L50.5765 39.6737L54.5672 54.5671L39.6739 50.5764L31.9647 63.9292L24.2554 50.5763L9.36203 54.567L13.3527 39.6737L0 31.9646L13.3529 24.2553L9.36229 9.36217L24.2554 13.3528L31.9646 0ZM31.8661 50.3147C41.946 50.3147 50.1174 42.1433 50.1174 32.0633C50.1174 21.9833 41.946 13.8119 31.8661 13.8119C21.7861 13.8119 13.6147 21.9833 13.6147 32.0633C13.6147 42.1433 21.7861 50.3147 31.8661 50.3147Z"/>
+            <circle cx="32" cy="32" r="14" fill="currentColor"/>
+        </svg>
+    </div>
+
+    <nav class="flex justify-center space-x-35 text-2xl font-regular">
+        <a href="#about"
+            class="min-w-[10ch] text-center rounded-full px-4 py-2 
+                    border-2 border-transparent
+                    transition-all duration-500
+                    hover:border-[var(--ocean-yellow)] 
+                    hover:text-[var(--ocean-hover)]">
+            About Me
+        </a>
+        <a href="#projects"
+            class="min-w-[10ch] text-center rounded-full px-4 py-2 
+                    border-2 border-transparent
+                    transition-all duration-500
+                    hover:border-[var(--ocean-yellow)] 
+                    hover:text-[var(--ocean-hover)]">
+            Projects
+        </a>
+        <a href="#contact"
+            class="min-w-[10ch] text-center rounded-full px-4 py-2 
+                    border-2 border-transparent
+                    transition-all duration-500
+                    hover:border-[var(--ocean-yellow)] 
+                    hover:text-[var(--ocean-hover)]">
+            Contact
+        </a>
+    </nav>
+    
+    <div class="w-12 h-12 text-[var(--white)] hover:text-[var(--ocean-yellow)] transition-colors duration-300 cursor-pointer">
+        <svg viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-full h-full">
+            <path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M31.9646 0L39.674 13.353L54.5671 9.36241L50.5765 24.2554L63.9292 31.9645L50.5765 39.6737L54.5672 54.5671L39.6739 50.5764L31.9647 63.9292L24.2554 50.5763L9.36203 54.567L13.3527 39.6737L0 31.9646L13.3529 24.2553L9.36229 9.36217L24.2554 13.3528L31.9646 0ZM31.8661 50.3147C41.946 50.3147 50.1174 42.1433 50.1174 32.0633C50.1174 21.9833 41.946 13.8119 31.8661 13.8119C21.7861 13.8119 13.6147 21.9833 13.6147 32.0633C13.6147 42.1433 21.7861 50.3147 31.8661 50.3147Z"/>
+            <circle cx="32" cy="32" r="14" fill="currentColor"/>
+        </svg>
+    </div>
+</header>
+
+<div class="flex-1 flex items-center justify-center ">
+    <h1 class="text-8xl -mt-32 font-bold title" in:fly={{ y: -50, duration: 2500, opacity: 0 }}>
+        Goncalo's World
+    </h1>
+</div>
+{/if}
+
+</section>
