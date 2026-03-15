886e3899d5feed41a61771b7e2b723c38d0e0bdf
goncalosamp27 <goncalo20.sampaio03@gmail.com>
2026-03-15 19:06:39 +0000

    Mobile menu changes + wave animation

--- src/lib/components/Contact.svelte
+++ src/lib/components/Contact.svelte
@@ -1,4 +1,4 @@
-<section id ="contact" class="h-screen flex flex-col items-center justify-center overflow-hidden" style="background-color: var(--bg-three)">
+<section id ="contact" class="h-screen flex flex-col items-center justify-center overflow-hidden" style="">
     <h1 class="text-3xl md:text-5xl font-bold text-center" style="color: var(--sky-grad)">
         Website area under development...
     </h1>
